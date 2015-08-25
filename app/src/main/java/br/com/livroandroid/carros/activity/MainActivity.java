5b241cf4f06c76ab20ba25c5179c5a95f4bc73cd
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-08-25 18:27:24 -0300

    Incluindo navigation drawer da design lib (google)

--- app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
+++ app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
@@ -2,36 +2,85 @@ package br.com.livroandroid.carros.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ListAdapter;
 
 import br.com.livroandroid.carros.R;
 import livroandroid.lib.fragment.NavigationDrawerFragment;
 
 public class MainActivity extends BaseActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    FrameLayout mContentFrame;
+
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+    private int mCurrentSelectedPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpToolbar();
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
-        //Nav Drawer
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (navigationView != null) {
-            setupDrawerContent(navigationView);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
+
+
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+            mFromSavedInstanceState = true;
         }
+
+        setUpNavDrawer();
+
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mContentFrame = (FrameLayout) findViewById(R.id.nav_contentframe);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                menuItem.setChecked(true);
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        Snackbar.make(mContentFrame, "Item One", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 0;
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.nav_messages:
+                        Snackbar.make(mContentFrame, "Item Two", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 1;
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.nav_friends:
+                        Snackbar.make(mContentFrame, "Item Three", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 2;
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.nav_discussion:
+                        Snackbar.make(mContentFrame, "Item Four", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 3;
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
     }
 
     @Override
@@ -56,33 +105,38 @@ public class MainActivity extends BaseActivity implements NavigationDrawerFragme
         return super.onOptionsItemSelected(item);
     }
 
+    protected void setUpToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+    }
+
+    private void setUpNavDrawer() {
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(R.drawable.ic_menu);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+        }
+    }
+
     @Override
-    public NavigationDrawerFragment.NavDrawerListView getNavDrawerView(NavigationDrawerFragment navigationDrawerFragment, LayoutInflater layoutInflater, ViewGroup container) {
-        //Deve retornar a view e o identificador do ListView
-        View view = layoutInflater.inflate(R.layout.nav_drawer_listview, container, false);
-        return new NavigationDrawerFragment.NavDrawerListView(view, R.id.listView);
+    public NavigationDrawerFragment.NavDrawerListView getNavDrawerView(NavigationDrawerFragment navigationDrawerFragment, LayoutInflater layoutInflater, ViewGroup viewGroup) {
+        return null;
     }
 
     @Override
     public ListAdapter getNavDrawerListAdapter(NavigationDrawerFragment navigationDrawerFragment) {
-        //Este método deve retornar o adapter que vai preencher o ListView
         return null;
     }
 
     @Override
     public void onNavDrawerItemSelected(NavigationDrawerFragment navigationDrawerFragment, int i) {
-        //Método chamado ao selecionar um item do ListView
-    }
 
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        return true;
-                    }
-                });
     }
 }
