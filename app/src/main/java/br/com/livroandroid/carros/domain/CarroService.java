aa3e123c74fa39d347527e1bbaa495a2dbf65719
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-09-13 16:05:19 -0300

    Buscando carros do JSON

--- app/src/main/java/br/com/livroandroid/carros/domain/CarroService.java
+++ app/src/main/java/br/com/livroandroid/carros/domain/CarroService.java
@@ -1,22 +1,72 @@
 package br.com.livroandroid.carros.domain;
 
 import android.content.Context;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.livroandroid.carros.R;
+import livroandroid.lib.utils.FileUtils;
 
 /**
  * Created by Guilherme on 06-Sep-15.
  */
 public class CarroService {
-    public static List<Carro> getCarros(Context context, String tipo){
+    private static final boolean LOG_ON = false;
+    private static final String TAG = "CarroService";
+
+    public static List<Carro> getCarros(Context context, String tipo) {
+        try {
+            String json = readFileFromTipo(context, tipo);
+            List<Carro> carros = parserJSON(context, json);
+            return carros;
+        } catch (Exception e) {
+            // TODO explicar exception
+            Log.e(TAG, "Erro ao ler os carros: " + e.getMessage(), e);
+            return null;
+        }
+    }
+
+    private static String readFileFromTipo(Context context, String tipo) throws IOException {
+        if ("classicos".equals(tipo)) {
+            return FileUtils.readRawFileString(context, R.raw.carros_classicos, "UTF-8");
+        } else if ("esportivos".equals(tipo)) {
+            return FileUtils.readRawFileString(context, R.raw.carros_esportivos, "UTF-8");
+        }
+        return FileUtils.readRawFileString(context, R.raw.carros_luxo, "UTF-8");
+    }
+
+    private static List<Carro> parserJSON(Context context, String json) throws IOException {
         List<Carro> carros = new ArrayList<Carro>();
-        for (int i = 0; i < 20; i++) {
-            Carro c = new Carro();
-            c.nome = "Carro " + tipo + ": " + i; //Nome dinâmico conforme o tipo
-            c.desc = "Desc " + i;
-            c.urlFoto = "http://www.livroandroid.com.br/livro/carros/esportivos/Ferrari_FF.png";
-            carros.add(c);
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONObject obj = root.getJSONObject("carros");
+            JSONArray jsonCarros = obj.getJSONArray("carro");
+            // Insere cada carro na lista
+            for (int i = 0; i < jsonCarros.length(); i++) {
+                JSONObject jsonCarro = jsonCarros.getJSONObject(i);
+                Carro c = new Carro();
+                // Lê as informações de cada carro
+                c.nome = jsonCarro.optString("nome");
+                c.desc = jsonCarro.optString("desc");
+                c.urlFoto = jsonCarro.optString("url_foto");
+                c.urlInfo = jsonCarro.optString("url_info");
+                if (LOG_ON) {
+                    Log.d(TAG, "Carro " + c.nome + " > " + c.urlFoto);
+                }
+                carros.add(c);
+            }
+            if (LOG_ON) {
+                Log.d(TAG, carros.size() + " encontrados.");
+            }
+        } catch (JSONException e) {
+            throw new IOException(e.getMessage(), e);
         }
         return carros;
     }
