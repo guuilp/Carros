45402fade32b043ef9ca0697b44e0cebb8db2d89
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-10-18 17:14:26 -0200

    Incluindo cache do json da lista de carros (salvando json na memoria interna)

--- app/src/main/java/br/com/livroandroid/carros/domain/CarroService.java
+++ app/src/main/java/br/com/livroandroid/carros/domain/CarroService.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 import br.com.livroandroid.carros.R;
 import livroandroid.lib.utils.FileUtils;
 import livroandroid.lib.utils.HttpHelper;
+import livroandroid.lib.utils.IOUtils;
 
 /**
  * Created by Guilherme on 06-Sep-15.
@@ -24,10 +26,46 @@ public class CarroService {
     private static final String URL = "http://www.livroandroid.com.br/livro/carros/carros_{tipo}.json";
 
     public static List<Carro> getCarros(Context context, String tipo) throws IOException {
+        List<Carro> carros = getCarrosFromArquivo(context, tipo);
+        if(carros != null && carros.size() > 0 ){
+            //Encontrou o arquivo
+            return carros;
+        }
+        //Se não encontrar, busca no webservice
+        carros = getCarrosFromWebService(context, tipo);
+        return carros;
+    }
+
+    //Abre o arquivo salvo, se existir, e cria a lista de carros
+    private static List<Carro> getCarrosFromWebService(Context context, String tipo) throws IOException{
         String url = URL.replace("{tipo}", tipo);
+        Log.d(TAG, "URL: " + url);
         String json = HttpHelper.doGet(url);
+        salvaArquivoNaMemoriaInterna(context, url, json);
         List<Carro> carros = parserJSON(context, json);
         return carros;
+    }
+
+    //Faz a requisição HTTP, cria a lista de carros e salva o JSON em arquivo
+    private static List<Carro> getCarrosFromArquivo(Context context, String tipo) throws IOException {
+        String fileName = String.format("carros_%s.json", tipo);
+        Log.d(TAG, "Abrindo arquivo: " + fileName);
+        //Lê o arquivo da memória interna
+        String json = FileUtils.readFile(context, fileName, "UTF-8");
+        if(json == null){
+            Log.d(TAG, "Arquivo " +fileName+ "não encontrado.");
+            return null;
+        }
+        List<Carro> carros = parserJSON(context, json);
+        Log.d(TAG, "Carros lidos do arquivo " +fileName+".");
+        return carros;
+    }
+
+    public static void salvaArquivoNaMemoriaInterna(Context context, String url, String json){
+        String fileName = url.substring(url.lastIndexOf("/")+1);
+        File file = FileUtils.getFile(context, fileName);
+        IOUtils.writeString(file, json);
+        Log.d(TAG, "Arquivo salvo: " + file);
     }
 
     private static String readFileFromTipo(Context context, String tipo) throws IOException {
